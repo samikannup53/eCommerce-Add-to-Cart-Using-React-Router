6979055fd1637e5c2378b90a736dba45242fe22a
samikannup53 <samikannup53@gmail.com>
2025-03-29 00:17:18 +0530

    Optimized Cart Page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,11 +3,6 @@ const Product = ({ product, cart, setCart }) => {
     product.title.length > 41
       ? product.title.substring(0, 40) + "..."
       : product.title;
-  
-  const description =
-    product.description.length > 61
-      ? product.description.substring(0, 60) + "..."
-      : product.description;
 
   const description =
     product.description.length > 61
